af18a67b134f73a60b281ae82001e84f7c2a02bc
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-12-07 22:57:55 -0300

    feat: implement URL shortening functionality with Base62 encoding and token management

---
+++ src/Services/UrlShortener/Shortix.UrlShortener.Core/ShortUrlGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Shortix.UrlShortener.Core.Extensions;
+
+namespace Shortix.UrlShortener.Core
+{
+    public class ShortUrlGenerator(TokenProvider tokenProvider)
+    {
+        public string GenerateUniqueUrl()
+        {
+            return tokenProvider.GetToken().EncodeToBase62();
+        }
+    }
+}
---
+++ src/Services/UrlShortener/Shortix.UrlShortener.Core/TokenRange.cs
@@ -0,0 +1,19 @@
+﻿namespace Shortix.UrlShortener.Core
+{
+    public sealed record TokenRange
+    {
+        public TokenRange(long start, long end)
+        {
+            if (end < start)
+            {
+                throw new ArgumentException("Start token cannot be greater than end token.", nameof(start));
+            }
+
+            Start = start;
+            End = end;
+        }
+
+        public long Start { get; }
+        public long End { get; }
+    }
+}
