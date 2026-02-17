f9d49955548ac9c04f4bca1a46c06054bb8ed026
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2026-02-17 15:43:19 -0300

    feat: initial redirect api setup

---
+++ src/Services/Redirect/Shortix.Redirect.WebApi/Configurations/ApiConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using MidR.DependencyInjection;
+using Shortix.Commons.Infrastructure;
+using Shortix.Commons.Infrastructure.Extensions;
+using Shortix.Redirect.WebApi.Configurations;
+using Shortix.TokenRange.WebApi.Configurations;
+using System.Reflection;
+
+namespace Shortix.Redirect.WebApi.Configurations
+{
+    public static class ApiConfiguration
+    {
+        extension(WebApplicationBuilder builder)
+        {
+            public WebApplicationBuilder AddApiConfiguration()
+            {
+                builder.AddCommonConfiguration();
+
+                builder.Services.AddMidR(Assembly.GetExecutingAssembly());
+
+                builder.AddSwaggerConfig();
+
+                builder.Services.AddEndpoints(typeof(ApiConfiguration).Assembly);
+
+                return builder;
+            }
+        }
+
+        extension(WebApplication app)
+        {
+            public WebApplication UseApiConfiguration()
+            {
+                app.UseCommonPipeline().UseSwaggerConfig();
+
+                return app;
+            }
+        }
+    }
+}
